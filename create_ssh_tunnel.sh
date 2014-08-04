2aaecbd130b7dcd1e186fa7452548894f318535f
Antonello Piemonte <apiemont@gmail.com>
2014-08-04 14:33:40 +0200

    create_ssh_tunnel.sh: initial commit

---
+++ create_ssh_tunnel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#########################
+## variables to update ##
+#########################
+
+REMOTE_HOST=ex-sol.mpe.mpg.de
+REVERSE_PORT=3310
+
+#############################################
+## there should be nothing to change below ##
+#############################################    
+
+TUNNEL_PID=$(pgrep -f "ssh -f -N -R $REVERSE_PORT:localhost:22")
+
+if [[ -z $TUNNEL_PID ]]; then
+        echo "starting tunnel .."
+        ssh -f -N -R $REVERSE_PORT:localhost:22 $REMOTE_HOST
+else
+        echo "tunnel already running (PID $TUNNEL_PID), exiting."
+        exit 1
+fi

88c2c3d9fa79e96dfa33909f48092c44ea94cf6a
Antonello Piemonte <apiemont@users.noreply.github.com>
2014-08-04 10:47:28 +0200

    phone.sh: added local cache usage when offline

--- phone.sh
+++ phone.sh
@@ -5,6 +5,7 @@
 #########################
 
 USERURL="http://www2011.mpe.mpg.de/interna/rumwusler-internal.php"
+LOCALCACHE=/tmp/mpeuserdb.txt
 
 #############################################
 ## there should be nothing to change below ##
@@ -13,14 +14,38 @@ USERURL="http://www2011.mpe.mpg.de/interna/rumwusler-internal.php"
 # usage blurb 
 [ $# -eq 0 ] && { echo " This scripts provides office phone number for MPE employees"; echo " Usage: $0 name/string"; exit 1; }  
 
-# check for links availability using builtin bash command
+# check for links text browser availability 
 command -v links &> /dev/null
 [ $? -gt 0  ] && { printf "Command links not found, please install with:\n 'sudo apt-get install links' (for Debian/Ubuntu)\n 'sudo yum install links' (for Fedora/RHEL)\n 'brew install wget' (for OSX, using http://brew.sh/)\n"; exit 1; }
 
-# dump the user data and return match ...
-echo "             name           |  phone             |  email                |  office       "
-echo "======================================================================================="
-links -width 140 -dump $USERURL | grep mpe.mpg.de | awk -F '|' '{print $2,$3,$5,$7,$8}' |  grep -i $1
-#links -dump $USERURL | grep mpe.mpg.de | awk -F '|' '{print $2,$3,$5}' |  grep -i $1
-# ... else return message
-[ $? -gt 0  ] && { echo "User not found or user has no valid MPE email address"; exit 1; }
+# check for user DB online availability, else use local cache, else fail and exit.
+HTTP_RETURN=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $USERURL)
+
+if (( $HTTP_RETURN == 200 )); then
+	# dump the user data, write out new cache file and return match ...
+	echo
+	echo "Using online user DB"
+	echo 
+	echo "             name           |  phone           |  email               |  office       "
+	echo "======================================================================================"
+	links -width 140 -dump $USERURL | grep mpe.mpg.de | awk -F '|' '{print $2,$3,$5,$7,$8}' | tee $LOCALCACHE |  grep -i $1
+	# ... else return message
+	[ $? -gt 0  ] && { echo "User not found or user has no valid MPE email address"; exit 1; }
+
+else
+	if [[ -f $LOCALCACHE ]] ; then
+		echo
+		echo "Online user DB unavailable, using local cache user DB."
+		echo 
+		echo "             name           |  phone           |  email               |  office       "
+		echo "======================================================================================"
+		grep mpe.mpg.de $LOCALCACHE | grep -i $1
+		# ... else return message
+		[ $? -gt 0  ] && { echo "User not found or user has no valid MPE email address"; exit 1; }
+	else
+		echo "Online user DB unavailable, local cache user DB unavailable"
+		echo "Run this script at least once when online user DB is available to cache results"
+		echo "Exiting ..."
+		exit 1
+	fi
+fi

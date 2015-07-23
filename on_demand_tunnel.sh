c49935e252270b14ed132793ababa9e0eda5ca72
Antonello Piemonte <apiemont@users.noreply.github.com>
2015-07-23 10:11:45 +0200

    Update on_demand_tunnel.sh
    minor fixes ...

--- on_demand_tunnel.sh
+++ on_demand_tunnel.sh
@@ -1,26 +1,27 @@
-#!/bin/bash
-
-#########################
-## variables to update ##
-#########################
-
-SUPPORT_USER=apiemont
-SUPPORT_EMAIL=apiemont@gmail.com
-REMOTE_HOST=host.example.com
-
-#############################################
-## there should be nothing to change below ##
-#############################################
-
-LOCK_FILE=~/.tunnel.lock
-TUNNEL_PUBKEY=~/.ssh/id_rsa_tunnel.pub
+#!/bin/bash                                                                                                                                                                                                                                                   
+                                                                                                                                                                                                                                                              
+###################################                                                                                                                                                                                                                           
+## variables to update if needed ##                                                                                                                                                                                                                           
+###################################                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                                              
+REMOTE_HOST=ex-sol.mpe.mpg.de                                                                                                                                                                                                                                 
+SUPPORT_USER=apiemont                                                                                                                                                                                                                                         
+SUPPORT_EMAIL=apiemont@gmail.com                                                                                                                                                                                                                              
+SUPPORT_PUBKEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCagVrIqctHf6lij69uTun2UmZ7qgAtY/2FCy39yir0GZbp/RZfOy0+9qm7KHn9G2PGM6ygY5bscMObKWXxJn+ZbctJkP9wpvprO9oIiRNWkDk7HPGDaIFq/E1N3PD6A7Ue/6/DvzETNIwJc1BgFfVlU19W4/dXPjR+1QO/RFtuh/LXmSWMG/G+jd4FCQHIXHun98g72VEB92X2WhNXFvAieWYCQjMwlLlPdedUavQBKDxtV/rdboFeDJ3GfKcWN/4AKg7uwmSkZAlgvMJPeGMT9bTpKv48eUd6REKEp1kwYqrQ+GxiPvpXCqVapC2PGUbr7aJIFeE/KQ9kF5a/z3/5 apiemont@ex-sol2"                                                                                              
+                                                                                                                                                                                                                                                              
+#############################################                                                                                                                                                                                                                 
+## there should be nothing to change below ##                                                                                                                                                                                                                 
+#############################################                                                                                                                                                                                                                 
+                                                                                                                                                                                                                                                              
+LOCK_FILE=~/.tunnel.lock                                                                                                                                                                                                                                      
+TUNNEL_PUBKEY=~/.ssh/id_rsa_tunnel.pub                                                                                                                                                                                                                        
 TUNNEL_PRIVKEY=~/.ssh/id_rsa_tunnel
-TUNNEL_PID=$(pgrep -f "$localhost:22" | paste -s)
+TUNNEL_PID=$(pgrep -f "$localhost:22" | paste -s) # paste, in case there are multiple pids ..)
 
 function tunnel_reset {
         [[ -r $LOCK_FILE ]] && { echo "Clearing tunnel lock file"; rm $LOCK_FILE; } || { echo "Tunnel lock file not found"; }
         [[ -r $TUNNEL_PUBKEY && -r $TUNNEL_PRIVKEY ]] && { echo "clearing tunnel keypair"; rm $TUNNEL_PUBKEY $TUNNEL_PRIVKEY; } || { echo "Tunnel keypair not found"; }
-        [[ -r ~/.ssh/authorized_keys ]] && { echo "Removing support public key from user account .. done"; sed -i.old  '/apiemont/d' ~/.ssh/authorized_keys; }
+        [[ -r ~/.ssh/authorized_keys ]] && { echo "Removing support public key from user account .. done"; sed -i.old  "/$SUPPORT_USER/d" ~/.ssh/authorized_keys; }
 }
 
 function tunnel_init {
@@ -30,7 +31,6 @@ function tunnel_init {
         else
                 echo "Requesting tunnel setup (initialization) for first time .."
                 touch $LOCK_FILE
-                [[ ! -d .ssh ]] && { echo "The .ssh directory is not existing, creating it .."; mkdir .ssh; chmod 700 .ssh; }
                 if [[ -r $TUNNEL_PUBKEY && -r $TUNNEL_PRIVKEY ]]; then
                         echo "Found tunnel keypair .."
                 else
@@ -43,11 +43,11 @@ function tunnel_init {
                 mailx -s "Tunnel init request from $USER@$HOSTNAME" -r $USER_EMAIL $SUPPORT_EMAIL < $TUNNEL_PUBKEY
 
                 # finally append support pub key to the requesting account, if not there already ..
-                if grep apiemont ~/.ssh/authorized_keys > /dev/null 2>&1; then
+                if grep $SUPPORT_USER ~/.ssh/authorized_keys > /dev/null 2>&1; then
                                 :
                         else
                                 printf "Adding support pub key to your account .. \nDone.\nWait for email confirmation from support that the tunnel is ready for usage before starting it (with --start)\n"
-                                echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCagVrIqctHf6lij69uTun2UmZ7qgAtY/2FCy39yir0GZbp/RZfOy0+9qm7KHn9G2PGM6ygY5bscMObKWXxJn+ZbctJkP9wpvprO9oIiRNWkDk7HPGDaIFq/E1N3PD6A7Ue/6/DvzETNIwJc1BgFfVlU19W4/dXPjR+1QO/RFtuh/LXmSWMG/G+jd4FCQHIXHun98g72VEB92X2WhNXFvAieWYCQjMwlLlPdedUavQBKDxtV/rdboFeDJ3GfKcWN/4AKg7uwmSkZAlgvMJPeGMT9bTpKv48eUd6REKEp1kwYqrQ+GxiPvpXCqVapC2PGUbr7aJIFeE/KQ9kF5a/z3/5 apiemont@ex-sol2" >> ~/.ssh/authorized_keys
+                                echo $SUPPORT_PUBKEY >> ~/.ssh/authorized_keys
                                 chmod 600 ~/.ssh/authorized_keys
                 fi
         fi
@@ -59,7 +59,7 @@ function tunnel_start {
                 echo "Starting tunnel .."
                 REVERSE_PORT=$((RANDOM%100+4000)) # random, within a range ..
                 ssh -i $TUNNEL_PRIVKEY -f -N -R $REVERSE_PORT:localhost:22 $SUPPORT_USER@$REMOTE_HOST
-                mailx -s "Tunnel started from $USER@$HOSTNAME" $SUPPORT_EMAIL <<< "Connect with 'ssh -p $REVERSE_PORT $USER@localhost"
+                mailx -s "Tunnel started from $USER@$HOSTNAME" $SUPPORT_EMAIL <<< "Connect with 'ssh -p $REVERSE_PORT $USER@localhost'"
         else
                 echo "Can't start tunnel, one is already running (PID $TUNNEL_PID), bye."
                 exit 1
